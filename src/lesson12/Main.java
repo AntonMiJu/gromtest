d8d526789e607f4f3cfa33d7468aba32082b13fa
AntonMiJu <anton.mi.ju@gmail.com>
2018-09-20 21:20:53 +0300

    init commit

---
+++ src/lesson12/Main.java
@@ -0,0 +1,22 @@
+package lesson12;
+
+public class Main {
+    public static void main(String[] args) {
+        Bank euBank = new EUBank(1222,"Sweden",Currency.EUR,100,1400,4,444343434);
+        Bank esBank = new USBank(1222,"Sweden",Currency.EUR,100,1400,4,444343434);
+        Bank chinaBank = new ChinaBank(1222,"Sweden",Currency.EUR,100,1400,4,444343434);
+        User user = new User(1001,"Denis",1200,40,"GMD",1500, euBank);
+        User user1 = new User(1002,"Andrey",12200,40,"GMD",2500, euBank);
+        User user2 = new User(1003,"Anton",10000,40,"GMD",1500, esBank);
+        User user3 = new User(1004,"Dima",222202,40,"GMD",500, esBank);
+        User user4 = new User(1005,"Jack",102200,40,"GMD",3500, chinaBank);
+        User user5 = new User(1006,"John",120200,40,"GMD",1500, chinaBank);
+
+        BankSystem bankSystem = new UkrainianBankSystem();
+        bankSystem.withdraw(user, 150);
+        bankSystem.fund(user,200);
+        bankSystem.transferMoney(user,user1,200);
+
+        System.out.println(user1.getBalance());
+    }
+}
---
+++ src/lesson12/UkrainianBankSystem.java
@@ -0,0 +1,64 @@
+package lesson12;
+
+public class UkrainianBankSystem implements BankSystem {
+    @Override
+    public void withdraw(User user, int amount) {
+        if (!checkWithdraw(user, amount))
+            return;
+        user.setBalance(user.getBalance() - amount - amount * user.getBank().getCommission(amount));
+    }
+
+    @Override
+    public void fund(User user, int amount) {
+        if (!checkFundLimits(user, amount, user.getBank().getLimitOfFunding()))
+            return;
+        user.setBalance(user.getBalance() + amount);
+    }
+
+    @Override
+    public void transferMoney(User fromUser, User toUser, int amount) {
+        if (!checkWithdraw(fromUser, amount))
+            return;
+        if (!checkFundLimits(toUser, amount, toUser.getBank().getLimitOfFunding()))
+            return;
+        //TODO check fund rules
+
+        fromUser.setBalance(fromUser.getBalance() - amount - amount * fromUser.getBank().getCommission(amount));
+        toUser.setBalance(toUser.getBalance() + amount - amount * fromUser.getBank().getCommission(amount));
+        //TODO fund
+    }
+
+    @Override
+    public void paySalary(User user) {
+        user.getBank().setTotalCapital(user.getBank().getTotalCapital()-user.getSalary());
+        user.setBalance(user.getBalance()+user.getSalary());
+    }
+
+    private boolean checkWithdraw(User user, int amount) {
+        return checkWithdrawLimits(user, amount, user.getBank().getLimitOfWithdrawal()) && checkWithdrawLimits(user, amount, user.getBalance());
+    }
+
+    private boolean checkWithdrawLimits(User user, int amount, double limit) {
+        if (amount + user.getBank().getCommission(amount) > limit) {
+            printWithdrawalErrorMsg(amount, user);
+            return false;
+        }
+        return true;
+    }
+
+    private boolean checkFundLimits(User user, int amount, double limit) {
+        if (amount > limit) {
+            printFundErrorMsg(amount, user);
+            return false;
+        }
+        return true;
+    }
+
+    private void printWithdrawalErrorMsg(int amount, User user) {
+        System.err.println("Can't withdraw money" + amount + "from " + user.toString());
+    }
+
+    private void printFundErrorMsg(int amount, User user) {
+        System.err.println("Can't fund money" + amount + "from " + user.toString());
+    }
+}
