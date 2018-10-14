677122a2c394eeea285ad6a464c11924d6ede233
AntonMiJu <anton.mi.ju@gmail.com>
2018-10-14 22:09:46 +0300

    lesson 16

---
+++ src/lesson16/homework/Demo.java
@@ -0,0 +1,5 @@
+package lesson16.homework;
+
+public class Demo {
+
+}
--- src/lesson16/homework/Solution.java
+++ src/lesson16/homework/Solution.java
@@ -1,12 +1,19 @@
 package lesson16.homework;
 
 public class Solution {
+    public static void main(String[] args) {
+        System.out.println(countWords("a1sf anton mi cosi dfsdg!s"));
+    }
+
     public static int countWords(String input){
         //65-122
+        if (input==null)
+            return 0;
         String[] array = input.split(" ");
         int counter = 0;
         int i = 0;
         for (String el:array){
+            i = 0;
             char[] mid = el.toCharArray();
             for (char el1:mid){
                 if (Character.isLetter(el1))
@@ -14,6 +21,7 @@ public class Solution {
             }
             if (i==mid.length)
                 counter++;
+
         }
         return counter;
     }
