896d9d2cee85d3cde1479ec183db51ee823c31d3
AntonMiJu <anton.mi.ju@gmail.com>
2018-10-15 00:33:57 +0300

    lesson 16

--- src/lesson16/homework/Solution2.java
+++ src/lesson16/homework/Solution2.java
@@ -1,30 +1,34 @@
 package lesson16.homework;
 
 public class Solution2 {
-    String maxWord(String input){
+    public static void main(String[] args) {
+        System.out.println(maxWord("a1sf anton mi cosi dfsdg!s"));
+        System.out.println(minWord("a1sf anton mi cosi dfsdg!s"));
+    }
+
+    private static String maxWord(String input) {
         int max = 0;
         int maxIndex = 0;
         int k = 0;
         String[] array = input.split(" ");
         String[] result = new String[array.length];
-        for (int i = 0; i<array.length;i++){
-            if (array[i]==null || array[i].isEmpty())
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == null || array[i].isEmpty())
                 continue;
-            int counter = 0;
             char[] mid = array[i].toCharArray();
-            for (char el1:mid){
+            for (char el1 : mid) {
                 if (Character.isLetter(el1))
                     i++;
             }
-            if (i==mid.length){
+            if (i == mid.length) {
                 result[k] = array[i];
                 k++;
             }
         }
-        for (int j = 0; j<result.length;j++){
-            if (result[j]==null || result[j].isEmpty())
+        for (int j = 0; j < result.length; j++) {
+            if (result[j] == null || result[j].isEmpty())
                 continue;
-            if (result[j].getBytes().length>max) {
+            if (result[j].toCharArray().length > max) {
                 max = result[j].toCharArray().length;
                 maxIndex = j;
             }
@@ -32,30 +36,29 @@ public class Solution2 {
         return result[maxIndex];
     }
 
-    String minWord(String input){
+    private static String minWord(String input) {
         int min = Integer.MAX_VALUE;
         int minIndex = 0;
         int k = 0;
         String[] array = input.split(" ");
         String[] result = new String[array.length];
-        for (int i = 0; i<array.length;i++){
-            if (array[i]==null || array[i].isEmpty())
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == null || array[i].isEmpty())
                 continue;
-            int counter = 0;
             char[] mid = array[i].toCharArray();
-            for (char el1:mid){
+            for (char el1 : mid) {
                 if (Character.isLetter(el1))
                     i++;
             }
-            if (i==mid.length){
+            if (i == mid.length) {
                 result[k] = array[i];
                 k++;
             }
         }
-        for (int j = 0; j<result.length;j++){
-            if (result[j]==null || result[j].isEmpty())
+        for (int j = 0; j < result.length; j++) {
+            if (result[j] == null || result[j].isEmpty())
                 continue;
-            if (result[j].getBytes().length<min) {
+            if (result[j].toCharArray().length < min) {
                 min = result[j].toCharArray().length;
                 minIndex = j;
             }
