0c13cd8f1c4764bbb9535e7f91c0698e806d47ca
AntonMiJu <anton.mi.ju@gmail.com>
2018-10-15 21:00:40 +0300

    lesson 16

--- src/lesson16/homework/Solution2.java
+++ src/lesson16/homework/Solution2.java
@@ -7,30 +7,14 @@ public class Solution2 {
     }
 
     private static String maxWord(String input) {
-        int max = 0;
+        int max = Integer.MIN_VALUE;
         int maxIndex = 0;
-        int k = 0;
-        String[] array = input.split(" ");
-        String[] result = new String[array.length];
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == null || array[i].isEmpty())
-                continue;
-            char[] mid = array[i].toCharArray();
-            int counter = 0;
-            for (char el1 : mid) {
-                if (Character.isLetter(el1))
-                    counter++;
-            }
-            if (counter == mid.length) {
-                result[k] = array[i];
-                k++;
-            }
-        }
+        String[] result = validate(input);
         for (int j = 0; j < result.length; j++) {
             if (result[j] == null || result[j].isEmpty())
                 continue;
             if (result[j].toCharArray().length > max) {
-                max = result[j].toCharArray().length;
+                max = result[j].length();
                 maxIndex = j;
             }
         }
@@ -40,6 +24,19 @@ public class Solution2 {
     private static String minWord(String input) {
         int min = Integer.MAX_VALUE;
         int minIndex = 0;
+        String[] result = validate(input);
+        for (int j = 0; j < result.length; j++) {
+            if (result[j] == null || result[j].isEmpty())
+                continue;
+            if (result[j].toCharArray().length < min) {
+                min = result[j].length();
+                minIndex = j;
+            }
+        }
+        return result[minIndex];
+    }
+
+    private static String[] validate(String input){
         int k = 0;
         String[] array = input.split(" ");
         String[] result = new String[array.length];
@@ -57,14 +54,6 @@ public class Solution2 {
                 k++;
             }
         }
-        for (int j = 0; j < result.length; j++) {
-            if (result[j] == null || result[j].isEmpty())
-                continue;
-            if (result[j].toCharArray().length < min) {
-                min = result[j].toCharArray().length;
-                minIndex = j;
-            }
-        }
-        return result[minIndex];
+        return result;
     }
 }
