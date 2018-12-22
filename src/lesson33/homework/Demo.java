1718aa5e7b47f3314d8892480c1f1f18f72a52b3
AntonMiJu <anton.mi.ju@gmail.com>
2018-12-22 20:50:34 +0200

    lesson 33

--- src/lesson33/homework/Demo.java
+++ src/lesson33/homework/Demo.java
@@ -1,8 +1,11 @@
 package lesson33.homework;
 
+import lesson33.ReadWriteFile;
+
 public class Demo {
     public static void main(String[] args) {
         WriteFromConsole.writeToFileFromConsole("/home/anton/Desktop/test.txt");
+        ReadWriteFile.writeFile("/home/anton/Desktop/test.txt");
         //WriteFromConsole.writeToFileFromConsole("/home/anton/Desktop/test1.txt");
     }
 }
