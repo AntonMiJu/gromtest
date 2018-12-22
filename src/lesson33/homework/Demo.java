e390e142855b5af707afeb7ec35920dcdb3fbf1c
AntonMiJu <anton.mi.ju@gmail.com>
2018-12-22 20:52:21 +0200

    lesson 33

--- src/lesson33/homework/Demo.java
+++ src/lesson33/homework/Demo.java
@@ -5,7 +5,7 @@ import lesson33.ReadWriteFile;
 public class Demo {
     public static void main(String[] args) {
         WriteFromConsole.writeToFileFromConsole("/home/anton/Desktop/test.txt");
-        ReadWriteFile.writeFile("/home/anton/Desktop/test.txt");
-        //WriteFromConsole.writeToFileFromConsole("/home/anton/Desktop/test1.txt");
+        WriteFromConsole.writeToFileFromConsole("/home/anton/Desktop/test1.txt");
+        ReadFromConsole.readFileByConsolePath();
     }
 }
